04ab83c7dc1ada3068c50b822b6b2522cb4fb8a0
kaiso <kaison54@hotmail.com>
2020-03-16 14:35:10 -0400

    added qr code generator

--- app/src/main/java/com/mycompany/signinapp/memberscreen.java
+++ app/src/main/java/com/mycompany/signinapp/memberscreen.java
@@ -1,14 +1,21 @@
 package com.mycompany.signinapp;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.client.Firebase;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 
 public class memberscreen extends AppCompatActivity {
 
@@ -23,6 +30,36 @@ public class memberscreen extends AppCompatActivity {
         TextView id = (TextView) findViewById(R.id.textView9);
         id.setText(tag);
 
+        //ImageView image_view = (ImageView) findViewById(R.id.imageView3);
+
+
+
+        QRCodeWriter writer = new QRCodeWriter();
+        try {
+            BitMatrix bitMatrix = writer.encode("https://www.youtube.com/", BarcodeFormat.QR_CODE, 512, 512);
+            int width = bitMatrix.getWidth();
+            int height = bitMatrix.getHeight();
+            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+                }
+            }
+            ((ImageView) findViewById(R.id.imageView3)).setImageBitmap(bmp);
+
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+
+
+//        try {
+//            // generate a 150x150 QR code
+//            Bitmap bm = encodeAsBitmap(barcode_content, BarcodeFormat.QR_CODE, 150, 150);
+//
+//            if(bm != null) {
+//                image_view.setImageBitmap(bm);
+//            }
+//        } catch (WriterException e) { //eek }
 
     }
 
